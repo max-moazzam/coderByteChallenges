4369ed24c1186dd6fa0a0aa8f5153ebd7c758204
Maxwell Moazzam <max.moazzam@gmail.com>
2015-03-13 13:56:50 -0700

    Delete 02firstFactorial.js

--- 02firstFactorial.js
+++
@@ -1,9 +0,0 @@
-//Function that finds the factorial of a parameter num
-
-function FirstFactorial(num) { 
-	var total = 1; //total variable declared to store the total
-  	for (i=num; i>0; i--) { //for loop which multiplies the total by a descending number(i.e. 4*3=12, 12*2=24, 24*1=24)
-      total *= i;
-    };
-  return total;          
-}
